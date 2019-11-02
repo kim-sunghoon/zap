2b885e4b8e5772908cb2b24068e01eeca0f4b725
kim-sunghoon <sunghoon1kim@postech.ac.kr>
2019-11-02 11:26:32 +0900

    csv_summary -- natsort를 이용하면 sorting이 쉬워지는구나 나중에 참고해야겠다

--- csv_summary.py
+++ csv_summary.py
@@ -2,6 +2,7 @@ import os
 from glob import glob
 import numpy
 import pandas as pd
+import natsort
 
 def read_csv(csv_name):
     return pd.read_csv(csv_name)
@@ -9,10 +10,13 @@ def read_csv(csv_name):
 if __name__ == "__main__":
     csv_lists_ = sorted(glob("./*/*.csv"))
     for csv_list in csv_lists_:
-        print(csv_list)
         if "summary_csv" in csv_list:
             del csv_lists_[csv_lists_.index(csv_list)]
+    #  print(csv_lists_)
+
+    csv_lists_ = natsort.natsorted(csv_lists_, reverse=False)
     print(csv_lists_)
+
     read_lists = []
     for csv_list in csv_lists_:
         readlines = read_csv(csv_list)
@@ -20,10 +24,10 @@ if __name__ == "__main__":
     for i in range(1, len(read_lists)):
         read_lists[0] = pd.merge(read_lists[0], read_lists[i], on=['mask', 'th'])
 
-    print(read_lists[0])
     headers = read_lists[0].columns.tolist()
+    print(headers)
 
-    #  read_lists[0].to_csv("summary_csv/summary.csv")
+    read_lists[0].to_csv("summary_csv/summary.csv")
 
 
 
